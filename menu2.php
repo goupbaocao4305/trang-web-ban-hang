cefe4ee15c8897a2076880cfa5b2f212c6f78bdc
Trần Quốc Nam <quocnam901@gmail.com>
2017-07-15 11:30:00 +0700

    admincp-modules

---
+++ menu2.php
@@ -0,0 +1,11 @@
+
+<div class="menu">
+      <ul>
+            <li><a href="../index.php">Trang chủ</a></li>   
+            <li><a href="index.php?quanly=quanlylich2&ac=dk" >Đăng ký thuê phòng họp</a></li> 
+            <li><a href="index.php?quanly=quanlylich3&ac=dk">Lịch phòng họp đã đăng ký</a></li>         
+            <li><a href="../index.php?xem=userinfo2&id=<?php echo $_SESSION['id']?>">Thông tin người dùng</a></li>  
+            <li><a href="../logout.php" style="color:#F00; text-decoration:none"><b>Logout</b></a></li>
+            
+      </ul>          
+      </div>
