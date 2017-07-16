3e4bed4a76a3c9e1c1ad2f4947a3cfef68e45f18
Trần Quốc Nam <quocnam901@gmail.com>
2017-07-16 18:01:57 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	  $tenmaychu='localhost';
-	  $tentaikhoan='root';
-	  $pass='';
-	  $csdl='taphoa';
-	  $conn=mysql_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Không kết nối được');
-	  mysql_select_db($csdl,$conn);
-?>
